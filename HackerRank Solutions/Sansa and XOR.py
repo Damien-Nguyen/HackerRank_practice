829885e0395899c9067f50482b79e2066dead2e3
Damien <DamienN18@hotmail.com>
2024-01-09 17:11:50 +1100

    solutions

---
+++ HackerRank Solutions/Sansa and XOR.py
@@ -0,0 +1,40 @@
+Sansa and XOR
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sansaXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def sansaXor(arr):
+    # Write your code here
+    if len(arr)%2 == 0:
+        return 0
+    result = 0
+    for i in range(0, len(arr), 2):
+        result ^=arr[i]
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = sansaXor(arr)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
