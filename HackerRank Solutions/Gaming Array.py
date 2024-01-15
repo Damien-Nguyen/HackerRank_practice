88bec5d985a87632c82c9166ca05eff1794a2d8c
Damien <DamienN18@hotmail.com>
2024-01-15 18:07:10 +1100

    answers

---
+++ HackerRank Solutions/Gaming Array.py
@@ -0,0 +1,46 @@
+Gaming Array 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gamingArray' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def gamingArray(arr):
+    # Write your code here
+    counter = -1
+    cmax = 0
+    
+    for i in range(len(arr)):
+        if cmax < arr[i]:
+            cmax = arr[i]
+            counter += 1
+            
+    if counter%2 == 0:
+        return 'BOB'
+    else:
+        return "ANDY"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    g = int(input().strip())
+
+    for g_itr in range(g):
+        arr_count = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = gamingArray(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
